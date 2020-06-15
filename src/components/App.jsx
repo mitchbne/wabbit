845c7afc85c356f71bd7274a74d4f715ad0e756b
Mitch Smith <mitchpsmith1998@gmail.com>
2020-06-15 13:04:59 +1000

    tidy up postcss config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import React from "react"
 const App = () => (
   <div className="flex flex-col bg-gray-900 h-screen w-screen items-center justify-center p-20">
     <div className="w-full md:w-1/2 ">
-      <img alt="logo" src={require("../images/logo.svg")} />
+      <img alt="logo" className="mx-auto" src={require("../images/logo.svg")} />
       <p className="text-center text-orange-400 text-3xl mt-16">Welcome to Wabbit!</p>
       <a className="text-white block w-full text-center text-lg" href="https://github.com/mitchbne/" rel="noopener noreferrer" target="_blank">@mitchbne</a>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,19 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
+  purge: [
+    "./src/**/*.jsx",
+    "./src/**/*.js",
+    "./public/index.html",
+  ],
   theme: {
     extend: {
       height: { "96": "32rem" },
       fontFamily: { sans: ["Inter var", ...defaultTheme.fontFamily.sans] },
+      // screens: {
+      // print: { raw: "print" }, // use utilities like print:text-black
+      // dark: { raw: "(prefers-color-scheme: dark)" }, // use utilities like dark:text-white
+      // },
     },
     container: { center: true },
   },
