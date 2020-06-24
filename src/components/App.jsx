e9be16e772d522dbd898d4560198fe450f129ab5
Mitch Smith <mitchpsmith1998@gmail.com>
2020-06-24 10:13:32 +1000

    redesign demo page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,11 @@
 import React from "react"
 
 const App = () => (
-  <div className="flex flex-col items-center justify-center w-screen h-screen p-20 bg-gray-900">
-    <div className="w-full md:w-1/2 ">
+  <div className="flex flex-col items-center justify-center w-screen h-screen py-20 px-4 bg-gray-050">
+    <div className="w-full max-w-2xl ">
       <img alt="logo" className="mx-auto" src={require("../images/logo.svg")} />
-      <p className="mt-16 text-3xl text-center text-orange-400">Welcome to Wabbit!</p>
-      <a className="block w-full text-lg text-center text-white" href="https://github.com/mitchbne/" rel="noopener noreferrer" target="_blank">@mitchbne</a>
+      <p className="mt-16 text-2xl font-medium text-center text-teal-400">Welcome to Wabbit!</p>
+      <a className="block w-full text-base text-center text-gray-400" href="https://github.com/mitchbne/" rel="noopener noreferrer" target="_blank">@mitchbne</a>
     </div>
   </div>
 )
