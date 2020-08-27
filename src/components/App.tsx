e186ee25fc7b6be844bb860ac813e9fdc2b22cc1
Mitch Smith <mitchpsmith1998@gmail.com>
2020-08-27 17:31:36 +1000

    fix contrast

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,7 +4,7 @@ const App = () => (
   <div className="flex flex-col items-center justify-center w-screen h-screen py-20 px-4 bg-gray-050">
     <div className="w-full max-w-2xl ">
       <img alt="logo" className="mx-auto h-12" src="https://tailwindui.com/img/tailwindui-logo.svg" />
-      <p className="mt-4 text-2xl text-center text-teal-400">Welcome to Wabbit!</p>
+      <p className="mt-4 text-2xl text-center text-teal-600">Welcome to Wabbit!</p>
       <a className="block w-full text-base text-center text-gray-600" href="https://github.com/mitchbne/" rel="noopener noreferrer" target="_blank">@mitchbne</a>
     </div>
   </div>
