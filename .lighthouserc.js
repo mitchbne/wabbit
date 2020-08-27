5f27b1746227b6f56ac7c8bfa2a8ae307bdf9b65
Mitch Smith <mitchpsmith1998@gmail.com>
2020-08-27 20:51:10 +1000

    update lighthouse scores

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -3,10 +3,10 @@ module.exports = {
     collect: { staticDistDir: "./build" },
     assert: {
       assertions: {
-        "categories:performance": ["warn", { minScore: 95 }],
-        "categories:accessibility": ["error", { minScore: 95 }],
-        "categories:seo": ["warn", { minScore: 95 }],
-        "categories:best-practices": ["error", { minScore: 95 }],
+        "categories:performance": ["warn", { minScore: 0.98 }],
+        "categories:accessibility": ["error", { minScore: 0.8 }],
+        "categories:seo": ["warn", { minScore: 0.98 }],
+        "categories:best-practices": ["error", { minScore: 0.98 }],
       },
     },
   },
